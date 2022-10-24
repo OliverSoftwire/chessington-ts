0ca9ab17c8f23cdd061da06be56138a3adee9d0c
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-24 11:37:05 +0100

    Apply prettier to test

--- src/engine/pieces/knight.test.ts
+++ src/engine/pieces/knight.test.ts
@@ -1,52 +1,52 @@
-import Board from '../board'
-import Player from '../player'
-import Square from '../square'
-import { Knight } from './knight'
-import { Pawn } from './pawn'
+import Board from "../board";
+import Player from "../player";
+import Square from "../square";
+import { Knight } from "./knight";
+import { Pawn } from "./pawn";
 
-describe('Knight', () => {
-  let board: Board
-  beforeEach(() => (board = new Board()))
+describe("Knight", () => {
+	let board: Board;
+	beforeEach(() => (board = new Board()));
 
-  it('can make knights moves', () => {
-    const knight = new Knight(Player.WHITE)
-    board.setPiece(Square.at(4, 4), knight)
+	it("can make knights moves", () => {
+		const knight = new Knight(Player.WHITE);
+		board.setPiece(Square.at(4, 4), knight);
 
-    const moves = knight.getAvailableMoves(board)
+		const moves = knight.getAvailableMoves(board);
 
-    const expectedMoves = [
-      Square.at(2, 5),
-      Square.at(2, 3),
-      Square.at(3, 6),
-      Square.at(3, 2),
-      Square.at(5, 6),
-      Square.at(5, 2),
-      Square.at(6, 5),
-      Square.at(6, 3),
-    ]
+		const expectedMoves = [
+			Square.at(2, 5),
+			Square.at(2, 3),
+			Square.at(3, 6),
+			Square.at(3, 2),
+			Square.at(5, 6),
+			Square.at(5, 2),
+			Square.at(6, 5),
+			Square.at(6, 3),
+		];
 
-    expect(moves).toEqual(expect.arrayContaining(expectedMoves))
-  })
+		expect(moves).toEqual(expect.arrayContaining(expectedMoves));
+	});
 
-  it('cannot make any other moves', () => {
-    const knight = new Knight(Player.WHITE)
-    board.setPiece(Square.at(4, 4), knight)
+	it("cannot make any other moves", () => {
+		const knight = new Knight(Player.WHITE);
+		board.setPiece(Square.at(4, 4), knight);
 
-    const moves = knight.getAvailableMoves(board)
+		const moves = knight.getAvailableMoves(board);
 
-    expect(moves).toHaveLength(8)
-  })
+		expect(moves).toHaveLength(8);
+	});
 
-  it('can jump over other pieces', () => {
-    const knight = new Knight(Player.WHITE)
-    const firstPawn = new Pawn(Player.WHITE)
-    const secondPawn = new Pawn(Player.BLACK)
-    board.setPiece(Square.at(4, 4), knight)
-    board.setPiece(Square.at(3, 4), firstPawn)
-    board.setPiece(Square.at(3, 5), secondPawn)
+	it("can jump over other pieces", () => {
+		const knight = new Knight(Player.WHITE);
+		const firstPawn = new Pawn(Player.WHITE);
+		const secondPawn = new Pawn(Player.BLACK);
+		board.setPiece(Square.at(4, 4), knight);
+		board.setPiece(Square.at(3, 4), firstPawn);
+		board.setPiece(Square.at(3, 5), secondPawn);
 
-    const moves = knight.getAvailableMoves(board)
+		const moves = knight.getAvailableMoves(board);
 
-    expect(moves).toContainEqual(Square.at(2, 5))
-  })
-})
+		expect(moves).toContainEqual(Square.at(2, 5));
+	});
+});
