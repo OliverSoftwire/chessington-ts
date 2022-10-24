55faa01c36b71d2f3d9df5c06b5898f533f5f4ef
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-24 12:09:27 +0100

    Basic king movement

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,18 @@
 import Board from "../board";
 import { Piece } from "./piece";
 import Square from "../square";
+import { isOnBoard } from "engine/moveHelpers";
+
+const MOVES = [
+	new Square(1, 0),
+	new Square(1, 1),
+	new Square(0, 1),
+	new Square(-1, 1),
+	new Square(-1, 0),
+	new Square(-1, -1),
+	new Square(0, -1),
+	new Square(1, -1),
+];
 
 export class King extends Piece {
 	constructor(player: symbol) {
@@ -8,6 +20,13 @@ export class King extends Piece {
 	}
 
 	getAvailableMoves(_board: Board): Square[] {
-		return [] as Square[];
+		const currentPosition = _board.findPiece(this);
+		return MOVES.map(
+			(move) =>
+				new Square(
+					currentPosition.row + move.row,
+					currentPosition.col + move.col,
+				),
+		).filter((move) => isOnBoard(move));
 	}
 }
