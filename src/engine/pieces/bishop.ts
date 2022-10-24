7276fd31fabe8934a0398fde523971c0e6d97151
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-24 11:25:42 +0100

    Add basic moves for bishops

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -7,7 +7,31 @@ export class Bishop extends Piece {
 		super(player);
 	}
 
-	getAvailableMoves(_board: Board) {
-		return [] as Square[];
+	getAvailableMoves(_board: Board): Square[] {
+		const currentPosition = _board.findPiece(this);
+		const moves: Square[] = [];
+
+		for (let offset = -7; offset < 8; offset++) {
+			if (offset === 0) continue;
+
+			moves.push(
+				new Square(
+					currentPosition.row + offset,
+					currentPosition.col + offset,
+				),
+				new Square(
+					currentPosition.row - offset,
+					currentPosition.col + offset,
+				),
+			);
+		}
+
+		return moves.filter(
+			(square) =>
+				square.row >= 0 &&
+				square.row < 8 &&
+				square.col >= 0 &&
+				square.col < 8,
+		);
 	}
 }
