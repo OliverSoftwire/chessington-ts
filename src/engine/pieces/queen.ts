fd1e7f2e6fc2e8f61b90c8170faf57433ae249ba
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-24 11:27:21 +0100

    Basic queen movement

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -9,6 +9,40 @@ export class Queen extends Piece {
 	}
 
 	getAvailableMoves(_board: Board): Square[] {
-		return [] as Square[];
+		const currentPosition = _board.findPiece(this);
+		const moves: Square[] = [];
+
+		for (let offset = -7; offset < 8; offset++) {
+			if (offset === 0) continue;
+
+			moves.push(
+				new Square(
+					currentPosition.row + offset,
+					currentPosition.col + offset,
+				),
+				new Square(
+					currentPosition.row - offset,
+					currentPosition.col + offset,
+				),
+			);
+		}
+
+		for (let row = 0; row < 8; row++) {
+			if (row === currentPosition.row) continue;
+			moves.push(new Square(row, currentPosition.col));
+		}
+
+		for (let col = 0; col < 8; col++) {
+			if (col === currentPosition.col) continue;
+			moves.push(new Square(currentPosition.row, col));
+		}
+
+		return moves.filter(
+			(square) =>
+				square.row >= 0 &&
+				square.row < 8 &&
+				square.col >= 0 &&
+				square.col < 8,
+		);
 	}
 }
