08f8d7ea5065555c51537b370c1ca704406c34b8
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-24 11:03:30 +0100

    Add basic movement to rook

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -9,6 +9,19 @@ export class Rook extends Piece {
 	}
 
 	getAvailableMoves(_board: Board): Square[] {
-		return [] as Square[];
+		const currentPosition = _board.findPiece(this);
+		const moves: Square[] = [];
+
+		for (let row = 0; row < 8; row++) {
+			if (row === currentPosition.row) continue;
+			moves.push(new Square(row, currentPosition.col));
+		}
+
+		for (let col = 0; col < 8; col++) {
+			if (col === currentPosition.col) continue;
+			moves.push(new Square(currentPosition.row, col));
+		}
+
+		return moves;
 	}
 }
