2471971f4b032c50b3d26232d1a2eac06e61e808
OliverSoftwire <115625185+OliverSoftwire@users.noreply.github.com>
2022-10-24 10:57:16 +0100

    Add two square move to first move on pawns

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,6 +4,8 @@ import { Piece } from "./piece";
 import Square from "../square";
 
 export class Pawn extends Piece {
+	hasMoved: boolean = false;
+
 	constructor(player: Player) {
 		super(player);
 	}
@@ -11,11 +13,29 @@ export class Pawn extends Piece {
 	getAvailableMoves(_board: Board): Square[] {
 		const currentSquare = _board.findPiece(this);
 
-		return [
+		const moves = [
 			new Square(
-				currentSquare.row + (this.player == Player.WHITE ? 1 : -1),
+				currentSquare.row +
+					(this.player === Player.WHITE ? 1 : -1),
 				currentSquare.col,
 			),
 		];
+
+		if (!this.hasMoved) {
+			moves.push(
+				new Square(
+					currentSquare.row +
+						(this.player === Player.WHITE ? 2 : -2),
+					currentSquare.col,
+				),
+			);
+		}
+
+		return moves;
+	}
+
+	moveTo(board: Board, newSquare: Square): void {
+		super.moveTo(board, newSquare);
+		this.hasMoved = true;
 	}
 }
